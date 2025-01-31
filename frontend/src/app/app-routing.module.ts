e7a8bd56ee1b37318595e289b5824eac1b005e0d
JohanR7 <143104864+JohanR7@users.noreply.github.com>
2025-01-31 22:14:03 +0530

    final for deploy

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ApiComponent } from './api/api.component';  // Import your components
 
-const routes: Routes = [
-  { path: '', component: ApiComponent },  // Default route
-];
+const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })],  // Add routes here
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
